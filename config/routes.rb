5d2c98aabaf3d44566dd763424502f03ea1af63f
kevin-wairi <kevinwairi@gmail.com>
2023-07-27 23:03:39 +0300

    updated Login create sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post '/login', to: 'sessions#create'
+  # get 'sessions#me'
+  delete '/logout',to: 'sessions#destroy'
   resources :carts
   resources :products
   resources :testimonials
