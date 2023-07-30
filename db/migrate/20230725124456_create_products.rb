2dcfed7cf05cf3077ae68979b04b85453a823b58
kush Muhamad <kushmohammed98@yahoo.com>
2023-07-30 20:25:07 +0300

    kush

--- db/migrate/20230725124456_create_products.rb
+++ db/migrate/20230725124456_create_products.rb
@@ -6,7 +6,6 @@ class CreateProducts < ActiveRecord::Migration[7.0]
       t.string :image
       t.string :description
       t.string :category
-      
       t.integer :user_id
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,6 @@ Product.delete_all
 # Seed data for products
 puts 'Seeding products...'
 product1 = Product.create(name: "Iphone 13", price: 600,  image: "https://i.imgur.com/5XfU5ZM.png", description: "Best camera in the industry", category: "Electronics", user_id: user1.id)
-product2 = Product.create(name: "Banana", price: 2,  image: "https://i.imgur.com/5XfU5ZM.png", description: "Enjoy your Fiber", category: "Food",user_id: user2.id)
+product2 = Product.create(name: "Banana", price: 2,  image: "https://i.imgur.com/5XfU5ZM.png", description: "Enjoy your Fiber", category: "Food",user_id: user2)
 
 puts "Database successfully seeded!"
