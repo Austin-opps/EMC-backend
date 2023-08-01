1635211827ebdf19112be31cf544d7969e85e7fe
Austin <akomen04@gmail.com>
2023-08-01 19:20:21 +0300

    Updated relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
  
-  # has_many :testimonials
-  # has_one :cart
-  # has_one_attached :profile_picture # Add this line to enable profile picture attachment
+  has_many :testimonials
+  has_one :cart
+  
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :email, presence: true
