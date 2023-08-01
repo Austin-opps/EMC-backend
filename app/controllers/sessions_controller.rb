ca27bd55d4ab31de286b5b3c82937b0add351792
kevin-wairi <kevinwairi@gmail.com>
2023-08-01 08:58:53 +0300

    updated session error rendering

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,9 @@ class SessionsController < ApplicationController
         session[:user_id] = user.id
         render json:user, status: :created
         else
-        render json: [error: "Invalid username of password"],status: :unauthorized
+        render json: [error: "Invalid username or password"],status: :unauthorized
       end
+      
   end
 
   def destroy
